03182b3728cde8722475cad6acf90a42cf8f0d40
Anet <Anet@A33COMP14.step.itstep.rv.ua>
2019-03-27 17:46:49 +0200

    Add mordochka

--- src/app/workers.service.ts
+++ src/app/workers.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Workers } from './workers';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WorkersService {
+  url = 'http://localhost:51123/Api/Workers';
+  httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json'}) };
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAllWorkers(): Observable<Workers[]> {
+    return this.http.get<Workers[]>(this.url + '/ReadAllWorkersInfo');
+  }
+
+  getWorkerById(WorkerId: string): Observable<Workers> {
+    return this.http.get<Workers>(this.url + '/ReadWorkerById/' + WorkerId);
+  }
+
+  createWorker(worker: Workers): Observable<Workers> {
+    return this.http.post<Workers>(this.url + '/CreateWorker/', worker, this.httpOptions);
+  }
+
+  updateWorker(worker: Workers): Observable<Workers> {
+    return this.http.put<Workers>(this.url + '/UpdateWorker/', worker, this.httpOptions);
+  }
+
+  deleteWorkerById(workerId: string): Observable<number> {
+    return this.http.delete<number>(this.url + '/DeleteWorkerById/' + workerId, this.httpOptions);
+  }
 }
--- src/app/workers.ts
+++ src/app/workers.ts
@@ -1,2 +1,7 @@
 export class Workers {
+    public Id: number;
+    public FirstName: string;
+    public LastName: string;
+    public Age: number;
+    public Salary: number;
 }
